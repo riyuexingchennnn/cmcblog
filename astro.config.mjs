3b6b0705b8135e14f6108b039a47be44082a3088
1287829233 <1287829233@qq.com>
2024-11-27 11:37:51 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = str => {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://riyuexingchennnn.github.io/',
-  base: '/dist',
+  base: '/',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
